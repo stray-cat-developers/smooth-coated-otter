7b21be02829ed15b8c9db01c1587cc0731820602
akiten-io <akiten@naver.com>
2022-06-08 21:10:10 +0900

    fix/app-environment
    fix boot run caused by multiple app environment instances error

--- src/main/kotlin/io/mustelidae/smoothcoatedotter/api/config/AppEnvironment.kt
+++ src/main/kotlin/io/mustelidae/smoothcoatedotter/api/config/AppEnvironment.kt
@@ -1,9 +1,7 @@
 package io.mustelidae.smoothcoatedotter.api.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 @ConfigurationProperties(prefix = "app")
 class AppEnvironment {
 
