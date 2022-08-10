875b87a255789af1c30df4cbcf74f7d038a010bc
stan.marsh <stan.marsh@sk.com>
2022-08-10 14:37:13 +0900

    chore: operation naming rule sample chore: error code naming rule sample

--- src/main/kotlin/io/mustelidae/smoothcoatedotter/api/domain/SampleController.kt
+++ src/main/kotlin/io/mustelidae/smoothcoatedotter/api/domain/SampleController.kt
@@ -4,15 +4,17 @@ import io.mustelidae.smoothcoatedotter.api.common.Reply
 import io.mustelidae.smoothcoatedotter.api.common.toReply
 import io.mustelidae.smoothcoatedotter.api.lock.EnableUserLock
 import io.mustelidae.smoothcoatedotter.api.permission.RoleHeader
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/sample")
+@RequestMapping("/v1/sample", "v1/gateway/sample")
 class SampleController {
 
+    @Operation(operationId = "Boilerplate.Sample.helloWorld", description = "Hello World")
     @EnableUserLock
     @GetMapping
     fun helloWorld(
