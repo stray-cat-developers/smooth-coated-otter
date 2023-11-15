a6f04265ae4fc8c3f9be8209cb68413a501ceb46
fennec-fox <elijah17@gmail.com>
2023-11-15 10:35:30 +0900

    define lib version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.2"
+    id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
     id("org.jmailen.kotlinter") version "3.14.0"
     id("com.avast.gradle.docker-compose") version "0.17.4"
@@ -25,29 +25,30 @@ repositories {
 ext["log4j2.version"] = "2.17.1"
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib:1.9.10"))
+    implementation(kotlin("reflect:1.9.10"))
 
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-undertow") {
+    implementation("org.springframework.boot:spring-boot-starter-actuator:3.1.5")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.5")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:3.1.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:4.1.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:3.1.5")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.1.5")
+    implementation("org.springframework.boot:spring-boot-starter-undertow:3.1.5") {
         exclude("io.undertow", "undertow-websockets-jsr")
     }
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.5") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         exclude(group = "junit", module = "junit")
     }
-    testImplementation("org.springframework.boot:spring-boot-starter-hateoas")
-    kapt("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-hateoas:3.1.5")
+    kapt("org.springframework.boot:spring-boot-configuration-processor:3.1.5")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.15.2")
 
     kapt("com.querydsl:querydsl-apt:${dependencyManagement.importedProperties["querydsl.version"]}:jakarta")
     implementation("com.querydsl:querydsl-jpa:${dependencyManagement.importedProperties["querydsl.version"]}:jakarta")
@@ -56,12 +57,13 @@ dependencies {
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
-    testImplementation("com.h2database:h2")
+    testImplementation("com.h2database:h2:2.2.224")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.3.0")
     testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:4.9.0")
     testImplementation("com.asarkar.spring:embedded-redis-spring:1.1.1")
 
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.1.3")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+    implementation("org.yaml:snakeyaml:2.0")
 
 }
 
