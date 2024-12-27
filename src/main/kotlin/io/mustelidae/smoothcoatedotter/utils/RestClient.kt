981dea1cb32024dcac040b63e754c4509f6bae21
fennec(서오석) <fennec.fox3@kakaostyle.com>
2024-12-27 14:51:57 +0900

    Change depreacted Code

--- src/main/kotlin/io/mustelidae/smoothcoatedotter/utils/RestClient.kt
+++ src/main/kotlin/io/mustelidae/smoothcoatedotter/utils/RestClient.kt
@@ -1,6 +1,7 @@
 package io.mustelidae.smoothcoatedotter.utils
 
 import io.mustelidae.smoothcoatedotter.api.config.AppEnvironment
+import org.apache.hc.client5.http.config.ConnectionConfig
 import org.apache.hc.client5.http.config.RequestConfig
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
 import org.apache.hc.client5.http.impl.classic.HttpClients
@@ -10,12 +11,19 @@ import java.util.concurrent.TimeUnit
 
 object RestClient {
     fun new(connInfo: AppEnvironment.ConnInfo): CloseableHttpClient {
+        val connectionConfig =
+            ConnectionConfig
+                .custom()
+                .setConnectTimeout(connInfo.connTimeout.toLong(), TimeUnit.SECONDS)
+                .setTimeToLive(TimeValue.ofSeconds(connInfo.connLiveDuration))
+                .build()
+
         val manager =
             PoolingHttpClientConnectionManagerBuilder
                 .create()
+                .setDefaultConnectionConfig(connectionConfig)
                 .setMaxConnPerRoute(connInfo.perRoute)
                 .setMaxConnTotal(connInfo.connTotal)
-                .setConnectionTimeToLive(TimeValue.ofSeconds(connInfo.connLiveDuration))
                 .build()
 
         return HttpClients
@@ -24,7 +32,6 @@ object RestClient {
             .setDefaultRequestConfig(
                 RequestConfig
                     .custom()
-                    .setConnectTimeout(connInfo.connTimeout.toLong(), TimeUnit.SECONDS)
                     .setResponseTimeout(connInfo.readTimeout * 2, TimeUnit.SECONDS)
                     .build(),
             ).build()
